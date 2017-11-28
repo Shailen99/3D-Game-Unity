0a9066e060c0b3e82240a97fea640230f8ff0f28
Shailen Sheth <sheths@asbindia.org>
2017-11-28 14:12:59 +0530

    Version 0.3
    - Added Bowling ball with physics and texture
    - Added box pyramid with Lenny Faces
    - Added Texture to the elevated platform.

--- Smuggler Space Game/Assets/Scripts/CameraController.cs
+++ Smuggler Space Game/Assets/Scripts/CameraController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+	public Transform Target;
+
+	public Vector3 Offset;
+
+	public bool useOffsetValue;
 	// Use this for initialization
 	void Start () {
-		
+		if (!useOffsetValue) {
+			Offset = Target.position - transform.position;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = Target.position - Offset;
+		transform.LookAt (Target);
 	}
 }
